ceecabeb55a1a5deb7506db2f895d8d8bdca3c6c
Jake Ward <jakenicholasward@gmail.com>
2024-12-04 01:00:55 +0000

    script fix

--- train_sae.sh
+++ train_sae.sh
@@ -2,7 +2,7 @@ pip install --upgrade numpy
 pip install datasets transformers sae-lens bitsandbytes natsort
 apt install build-essential -y
 
-python3 save_gemma_acts_and_loss.py
-./run_all.sh
+python3 save_gemma_acts_and_loss.py --layers 12
+./scripts/run_all.sh
 
 python3 train_on_error.py
